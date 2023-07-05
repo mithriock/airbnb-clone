83d9f2e1086dab8017909d1158c0f81a03a03d23
Rodrigo M <rodrigo.munoz@ulinksolution.com>
2023-07-05 00:45:29 -0400

    avances en navbar

---
+++ test/logo.test.ts
@@ -0,0 +1,20 @@
+import Logo from '../app/components/navbar/Logo';
+
+// // Tests that Image component renders with correct props
+describe('Test Logo component', () => {
+  it('test_image_renders_with_correct_props', () => {
+      expect(4).toBe(4)
+  });
+  it('test_image_renders_with_correct_props_1', () => {
+      expect(4).toBe(4)
+  });
+  it('test_image_renders_with_correct_props_2', () => {
+      expect(4).toBe(4)
+  });
+  it('test_image_renders_with_correct_props_3', () => {
+      expect(4).toBe(4)
+  });
+  it('test_image_renders_with_correct_props_4', () => {
+      expect(4).toBe(4)
+  });
+});
